19188f9b737a78db11b778730b6c4eecbe916637
Tarekmohamed97 <tm7271420@gmail.com>
2020-11-04 22:37:27 +0200

    modefying the index.js file in the backend side, and adding a .gitignore file

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = reuqire('cors');
+const cors = require('cors');
 require('dotenv').config();
 
 //setup express
@@ -8,3 +8,24 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`server has started on port: ${PORT}`);
+});
+
+//setup mongoose
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, err => {
+    if(err) throw err;
+    console.log("mongodb connection established")
+})
+
+//setup routes
+
+app.use("/users", require("./routes/userRoutes"))
