59462fa7d736a913ca1d7c033e99cf345ccb85ec
Tarekmohamed97 <tm7271420@gmail.com>
2020-11-08 23:28:59 +0200

    modefying the app.js file, adding the api requests needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,60 @@
+import React , {useState, useEffect} from 'react';
+import Axios from 'axios'
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Header from './components/layout/header'
+import Home from './components/pages/home';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
+import {UserContext} from './context/userContext'
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined
+  })
+
+  
+
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if(token === null){
+        localStorage.setItem("auth-token", "");
+        token = ""
+      }
+      const tokenRes = await Axios.post(
+        "http://localhost:5000/users/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if(tokenRes.data){
+        const userRes = await Axios.get("http://localhost:5000/users",
+          { headers: { "x-auth-token": token } },
+        )
+        setUserData({
+          token,
+          user: userData.data
+        })
+      }
+      console.log(tokenRes.data)
+    }; 
+
+    isLoggedIn();
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value = {{userData, setUserData}}>
+        <Header />
+        <Switch>
+          <Route path = "/"  exact component = {Home} />
+          <Route path = "/login" exact component = {Login} />
+          <Route path = "/register" exact component = {Register} />
+        </Switch>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
