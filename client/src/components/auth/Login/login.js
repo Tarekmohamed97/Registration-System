d1da7ccd5cdea479c139fb24386578bc358146d8
Tarekmohamed97 <tm7271420@gmail.com>
2020-11-10 22:35:31 +0200

    modefying the login page to add the functionalities

--- client/src/components/auth/Login/login.js
+++ client/src/components/auth/Login/login.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, {useContext, useState} from 'react';
+import axios from 'axios';
+import '../Register/register.css';
+import {UserContext} from '../../../context/userContext'
+import { useHistory } from 'react-router-dom';
+
 
 function Login() {
+
+    const [state, setState] = useState({
+        email: "",
+        password: ""
+    })
+
+    const {setUserData} = useContext(UserContext);
+    const history = useHistory()
+
+    const handleChange = (e) => {
+        setState({
+            ...state,
+            [e.target.id ]: e.target.value
+        })
+    }
+
+    const submitForm = async (e) => {
+        e.preventDefault()
+        const {email, password} = state;
+        const loginRes = await axios.post('http://localhost:5000/users/login', {email, password});
+        setUserData({
+            token: loginRes.data.token,
+            user: loginRes.data.user
+        });
+        localStorage.setItem('auth-token', loginRes.data.token);
+        history.push('/');
+    };
+
+
+
     return (
-        <div>
-            Login
+        <div style = {{width: '100%'}}>
+            <div className = "form__Container">
+                <h1>Log in</h1>
+                <form onSubmit = {submitForm} className = "inputs__container">
+                    <div className = "inputForm__section">
+                        <label>email:</label>
+                        <input id = "email" type = "email" onChange = {(e) => handleChange(e)} />
+                    </div>
+                    <div className = "inputForm__section">
+                        <label>Password:</label>
+                        <input id = "password" type = "password" onChange = {(e) => handleChange(e)}/>
+                    </div>
+                    <div>
+                        <button className = "submitButton__form" type = "submit">Log in</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
