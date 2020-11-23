e5f7c56c1b43b8ff25aec2a50f1eb5d8021d2d97
Tarekmohamed97 <tm7271420@gmail.com>
2020-11-23 18:04:07 +0200

    mdoefying the user profile component

--- client/src/components/pages/profile/profile.js
+++ client/src/components/pages/profile/profile.js
@@ -1,5 +1,8 @@
 import React, {useContext} from 'react';
-import {UserContext} from '../../../context/userContext'
+import {UserContext} from '../../../context/userContext';
+import {Link} from 'react-router-dom'
+import './profile.css';
+import Unknown from '../../../images/Unknown_person.jpg';
 
 function Profile() {
 
@@ -7,9 +10,25 @@ function Profile() {
 
     return (
         <div>
-            {userData.user? <p>{userData.user.displayname}</p> : null }
+            <div className = "profileCard__container">
+                <div className = "profileCard__Image__section">
+                    <img src = {Unknown} alt = "unknown"/>
+                </div>
+                <div className = "userData__section">
+                    {userData.user? <p>{userData.user.displayname}</p> : null }
+                </div>
+                <div className = "userButtons__Section">
+                    <Link to = "/">
+                        <button className = "userButton" >Home</button>
+                    </Link>
+                    <button className = "userButton">Start Quiz</button>
+                    <button className = "userButton">My Scores</button>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Profile
+
+
